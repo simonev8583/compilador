5ccd877bd44cf2abf49768b29e31b99e39ebb61b
Camilo Lopez <camilopezc@hotmail.com>
2018-10-31 16:49:28 -0500

    Limpiar tablas

--- Transversal/ManejadorErrores.cs
+++ Transversal/ManejadorErrores.cs
@@ -70,11 +70,7 @@ namespace Compilador.Transversal
         public void limpiarManejadorErrores()
         {
             mapaErrores.Clear();
-            //this.mapaErrores= new Dictionary<string, List<Error>>();
-            Console.WriteLine(this.ObtenerTodo());
-            //mapaErrores["LEXICOS"].Clear();
-            //mapaErrores["SINTACTICO"].Clear();
-            //mapaErrores["SEMANTICO"].Clear();
+            INSTANCIA = new ManejadorErrores();
         }
     }
 }
